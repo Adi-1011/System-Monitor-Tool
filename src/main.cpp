f2fcbd8bfa0ddf7d6c085832e4081f00ef240ae1
Adi-1011 <sonu10112002@gmail.com>
2025-11-09 08:25:37 +0000

    Day 5: Implement real time auto-refresh and user triggered process termination

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,13 @@ using namespace std;
 
 int main() {
     SystemMonitor monitor;
+    int refreshRate = 2;
     // this monitor the resources and usage for upto total i updates
-
+    while (true){
     for (int i = 0; i < 3; ++i) { // temporary 5 updates for test
         system("clear");
         monitor.display();
-        this_thread::sleep_for(std::chrono::seconds(2));
+        std::this_thread::sleep_for(std::chrono::seconds(refreshRate));
     }
 
 
@@ -25,15 +26,20 @@ int main() {
 
     // implementation to kill process based on PID
     int pid;
-    cout << "\n ENTER PID to kill (or 0 to skip): ";
+    cout << "\n ENTER PID to kill (or -1 to skip): ";
     cin >> pid;
 
-    if(pid > 0){
-        monitor.killProcess(pid);
-    }
-    else {
+    if(pid == -1 ){
         cout << "No process terminated.\n";
+        
+    }
+    else if (pid > 0) {
+        monitor.killProcess(pid);
+        cout << "Returning to live preview..\n";
+        std::this_thread::sleep_for(std::chrono::seconds(2));
     }
 
-    return 0;
+   
+}
+ return 0;
 }
